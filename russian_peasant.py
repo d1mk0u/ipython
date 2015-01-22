9ec3c79c51fc23487d40bb6a24d32334e6d87062
DmitriS <dimheld@gmail.com>
2015-01-22 23:41:18 +0200

    Update russian_peasant.py

--- russian_peasant.py
+++ russian_peasant.py
@@ -1,25 +1,28 @@
 ## Russian Peasant multiplication
-## Author DmitriS
-def ruspeace(a, b):
-    if a == 1:
-        a = a * b
-        return a
-    elif a < b:
-        a, b = b, a
-        print(a, b)
-    z = []
-    num = 0
-    while a >= 1:
-        a = a // 2
-        b = b * 2
-        
-        print(a, b)
-        if a % 2 == 1:
-            z.append(b)
-    for i in z:
-        num = num + i
-    return num
+## Russian Peasant multiplication
+while True:
+    def ruspeace(a, b):
+        if a == 1:
+            a = a * b
+            print("a * b equal", a)
+            return "It Works"
+        elif a < b:
+            a, b = b, a
+            #print(a, b)
+        z = []
+        num = 0
+        while a >= 1:
+            a = a // 2
+            b = b * 2
+            
+            #print(a, b)
+            if a % 2 == 1:
+                z.append(b)
+        for i in z:
+            num = num + i
+        print("a * b equal to", num)
+        return "It Works"
 
-a = input("Insert Your first number: ")
-b = input("Insert Your second number: ")
-print(ruspeace(a, b))
+    a = int(input("Insert Your first number: "))
+    b = int(input("Insert Your second number: "))
+    print(ruspeace(a, b))
