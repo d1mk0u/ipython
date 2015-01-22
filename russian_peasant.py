00ca6c94a73799d686a2b57cb7631ba102f4a6e5
DmitriS <dimheld@gmail.com>
2015-01-22 23:24:34 +0200

    Update russian_peasant.py

--- russian_peasant.py
+++ russian_peasant.py
@@ -2,16 +2,21 @@ def ruspeace(a, b):
     if a == 1:
         a = a * b
         return a
+    elif a < b:
+        a, b = b, a
+        print(a, b)
     z = []
     num = 0
     while a >= 1:
         a = a // 2
         b = b * 2
-##        print(a, b)
+        
+        print(a, b)
         if a % 2 == 1:
             z.append(b)
     for i in z:
         num = num + i
     return num
-ax = ruspeace(2,100)
+
+ax = ruspeace(23,1004)
 print(ax)
