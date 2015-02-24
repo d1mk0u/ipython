a15d089bf39384fecb8d55aedc324f3bdbfe0397
DmitriS <dimheld@gmail.com>
2015-02-24 23:03:01 +0200

    Update est_palk_func2.py

--- est_palk_func2.py
+++ est_palk_func2.py
@@ -11,10 +11,15 @@ def tootaja_kogumipension(brutto_palk):
 def tulumaks(brutto_palk):
     x = (brutto_palk - tootaja_kogumipension(brutto_palk) - tootaja_tootukindlustusmaks(brutto_palk) - 154.0) * 0.2
     return x
-
+def palgafond(brutto_palk):
+    kogupalk = brutto_palk + (brutto_palk * 0.33 + brutto_palk * 0.008)
+    return kogupalk
 def arvutus():
     brutto_sum = int(raw_input("Kirjua oma brutto palk: "))
     netto = brutto_sum - tootaja_kogumipension(brutto_sum) - tootaja_tootukindlustusmaks(brutto_sum) - tulumaks(brutto_sum)
+    print "Palgafond:", palgafond(brutto_sum)
+    print "Brutto palk:", brutto_sum
     return netto
+
 x = arvutus()
 print "Netto palk", x
